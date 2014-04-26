b7a0b21e89b8a34d44072ca68a597a998b762de2
Mao Rui <mr.fanyange@gmail.com>
2014-04-27 00:42:51 +0800

    Many changes, mainly complete the news feed sections on homepage.
    1. Use nokgogiri to scrap news links from the original page.
    1. Use gridism and normalize to make css layouts easy.
    1. Adjust many html elements and css effect.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
+require 'open-uri'
 class StaticPagesController < ApplicationController
   def home
+    pages = []
+    [1,2,3,5,6].each { |i| pages << Nokogiri::HTML(open("http://rosefench.net/news.php?lb=#{i}")) }
+    @lists = []
+    pages.each do |page|
+      @lists << page.css('.line a')
+    end
+    @names = %W[快讯 鸟会动态 项目 鸟人学堂 鸟类观察]
   end
 end
