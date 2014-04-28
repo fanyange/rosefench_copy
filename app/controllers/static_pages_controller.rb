fa1e24fc5acd84be8f742379e9982c6b610b8cd8
Mao Rui <mr.fanyange@gmail.com>
2014-04-28 23:55:20 +0800

    Finish section show action; create banner

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,9 @@ require 'open-uri'
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def section_page
+    @section = Section.find(params[:id])
+    @request = request.original_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  match '/sections/:id', to: 'static_pages#section_page', via: 'get', as: 'section_path'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
