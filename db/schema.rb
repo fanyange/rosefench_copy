08f7f1c78abd7d0051290e8bdaf72b379a5e3061
Mao Rui <mr.fanyange@gmail.com>
2014-05-02 23:22:51 +0800

    Finish user sign up page and its tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429143810) do
+ActiveRecord::Schema.define(version: 20140502142726) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -38,5 +38,7 @@ ActiveRecord::Schema.define(version: 20140429143810) do
     t.datetime "updated_at"
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,65 @@ describe "User Pages" do
     before { visit signup_path }
     it { should have_selector('h1', text: "Sign up") }
   end
+
+  describe "sign up" do
+    let(:submit) { "Create an account" }
+    before do 
+      @user = User.new(email: "mr.fanyange@gmail.com", name: "fanyange", password: "foobar", password_confirmation: "foobar")
+      visit signup_path
+    end
+    
+    describe "with invalid user" do
+      before do
+        fill_in "Email", with: " "
+        fill_in "Name", with: @user.name
+        fill_in "Password", with: @user.password
+        fill_in "Password confirmation", with: @user.password_confirmation
+        click_button submit
+      end
+
+      it { should have_selector('h1', "Sign up") }
+      it { should have_selector('div.field_with_errors') }
+      it { should have_selector('.error-details') }
+    end
+
+    describe "with the same user" do
+      before do
+        @user.save
+        fill_in "Email", with: @user.email
+        fill_in "Name", with: @user.name
+        fill_in "Password", with: @user.password
+        fill_in "Password confirmation", with: @user.password_confirmation
+      end
+
+      it "should not create a new user" do
+        expect do
+          begin
+            click_button submit
+          rescue ActiveRecord::RecordNotUnique
+          end
+        end.not_to change(User,:count)
+      end
+    end
+
+    describe "with valid user" do
+      before do
+        fill_in "Email", with: @user.email
+        fill_in "Name", with: @user.name
+        fill_in "Password", with: @user.password
+        fill_in "Password confirmation", with: @user.password_confirmation
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create an account" }
+
+        it { should have_selector('.success', 'Your account has successfully created!') }
+        it { should have_text(@user.name) }
+      end
+    end
+  end
 end
